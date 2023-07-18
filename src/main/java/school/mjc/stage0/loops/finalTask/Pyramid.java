c3c133c25f3cc75ea77252ae263f5200e1c374f3
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-18 15:15:31 +0600

    Module #4.2. Final Task(v2 maven-surefire-plugin 2.19.1)

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= (cathetusLength - i) * 2; j++)
+            for (int j = 1; j <= cathetusLength - i; j++)
                 System.out.print(" ");
             for (int k = i; k >= 1; k--)
-                System.out.print(" " + k);
+                System.out.print("" + k);
             for (int l = 2; l <= i; l++)
-                System.out.print(" " + l);
+                System.out.print("" + l);
             System.out.println();
         }
     }
